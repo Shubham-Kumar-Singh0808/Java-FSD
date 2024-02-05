038be5795e2a247306772b0f5c6f62fbf7bfd755
sushil <sushil_tsk@yahoo.com>
2024-02-05 19:19:27 +0530

    Implement Spring Security with Authentication

---
+++ src/Phase3/authentication/authentication/src/main/java/com/example/authentication/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.authentication;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfiguration {
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(new CustomAuthenticationProvider());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/public/**").permitAll()
+            .anyRequest().authenticated()
+            .and().formLogin();
+    }
+}
+
