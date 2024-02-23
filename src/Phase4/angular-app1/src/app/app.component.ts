dce90549cd8b28c7f99e12ed9e9556223ad9a4b5
Shubham Kumar Singh <shubhamkumarsinghbxr@gmail.com>
2024-02-23 23:43:53 +0530

    3.2 Property Binding

---
+++ src/Phase4/angular-app1/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template:`
+  <product-list></product-list>
+  `,
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular-app1';
+}
